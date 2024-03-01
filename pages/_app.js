8e9810b647b748e9f3494879729aa562202fc4c9
Wenzhao Hu <wzhudev@gmail.com>
2024-03-01 10:27:58 +0800

    docs: upgrade nextra add update debug.mdx

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import 'nextra-theme-docs/style.css'
-
-export default function Nextra({ Component, pageProps }) {
+import '../styles.css'
+ 
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- theme.config.js
+++ theme.config.js
@@ -1,92 +1,38 @@
-import { useRouter } from "next/router";
-
 export default {
-  github: 'https://github.com/wzhudev/redi',
   docsRepositoryBase: 'https://github.com/wzhudev/redi-site/tree/main',
-  titleSuffix: ' – redi',
-  floatTOC: true,
-  logo: () => {
-    const TITLE_WITH_TRANSLATIONS = {
-      "zh-CN": "轻量的依赖注入框架",
-      "en-US": "a lightweight dependency injection library"
-    };
-    const { locale } = useRouter();
-    return <>
-      <span className="mr-2 font-extrabold hidden md:inline">redi</span>
-      <span className="text-gray-600 font-normal hidden md:inline">
-        {TITLE_WITH_TRANSLATIONS[locale]}
-      </span>
-    </>
+  project: {
+    link: 'https://github.com/wzhudev/redi',
   },
-  head: (
+  logo: (
     <>
-      <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta name="theme-color" content="#ffffff" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="redi: a light weight dependency injection library, along with bindings for React." />
-      <meta name="twitter:card" content="summary_large_image" />
-      {/* <meta name="twitter:image" content="https://nextra.vercel.app/og.png" /> */}
-      <meta name="twitter:site:domain" content="redi.wendellhu.fun" />
-      <meta name="twitter:url" content="https://redi.wendellhu.fun" />
-      <meta name="og:description" content="redi: a light weight dependency injection library, along with bindings for React." />
-      <meta name="og:title" content="redi: dependency injection for JavaScript" />
-      {/* <meta name="og:image" content="https://nextra.vercel.app/og.png" /> */}
-      <meta name="apple-mobile-web-app-title" content="redi" />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-icon-180x180.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="192x192"
-        href="/android-icon-192x192.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="96x96"
-        href="/favicon-96x96.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+      <span style={{ marginLeft: '.4em', fontWeight: 800 }}>Redi</span>
     </>
   ),
-  search: true,
-  prevLinks: true,
-  nextLinks: true,
-  footer: true,
-  footerEditLink: ({ locale }) => {
-    switch (locale) {
-      case "zh-CN":
-        return "在 GitHub 上编辑本页";
-      case "es-ES":
-        return "Edite esta página en GitHub";
-      case "ja":
-        return "Github で編集する";
-      case "ko":
-        return "Github에서 이 페이지 편집하기";
-      default:
-        return "Edit this page on GitHub";
-    }
-  },
-  footerText: <>MIT {new Date().getFullYear()} © wzhudev.</>,
-  unstable_faviconGlyph: '👋',
+    head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Redi" />
+      <meta property="og:description" content="A light-weight dependency injection library" />
+    </>
+  ),
   i18n: [
     { locale: "en-US", text: "English" },
     { locale: "zh-CN", text: "简体中文" },
   ],
+  toc: {
+    title: '',
+    backToTop: true
+  },
+  footer: {
+    text: (
+      <span>
+        MIT {new Date().getFullYear()} ©{' '}
+        <a href="https://github/wzhudev" target="_blank">
+           Wenzhao Hu 
+        </a>
+        .
+      </span>
+    )
+  },
+  faviconGlyph: "💉"
 }
